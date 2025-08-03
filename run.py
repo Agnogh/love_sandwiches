71aaba4fcdba149f490ab1a15d06b8f063353fd0
Agnogh <agnogh_orion@yahoo.com>
2025-08-04 01:31:58 +0200

    create get sales data function to get data strings from user

--- run.py
+++ run.py
@@ -25,21 +25,36 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 # gspread.authorize() = Logs into Google Sheets
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-# this is "command" to open spreadsheet. this is the name of the 
+# this is "command" to open spreadsheet. this is the name of the
 # spreadsheet
 # .open('love_sandwiches') = Opens spreadsheet by that name
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 # we are specifying tab names "sales"
-sales = SHEET.worksheet('sales')
+# sales = SHEET.worksheet('sales')
 
-# printing the list of tabs of that spreadsheet - 
+# printing the list of tabs of that spreadsheet -
 # just to Check if setup is OK
-print(SHEET.worksheets())
+# print(SHEET.worksheets())
 
 # picks up all the data from "sales" part of the sptreadsheet
 # and then returns big list of rows and colums
-data = sales.get_all_values()
+# data = sales.get_all_values()
 
 # prints out spreadsheet data in terminal (in list form)
-print(data)
+# print(data)
+
+
+def get_sales_data():
+    """
+    get sales figures input from user
+    """
+    print("Please enter sales data from the last market")
+    print("Data should be 6 number seperated by commas")
+    print("Example: 10. 20, 30, 40, 50, 60\n")
+
+    data_str = input("Enter your Data here: ")
+    print(f'The Data provided in the {data_str}')
+
+
+get_sales_data()
