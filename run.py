fca1e60acb225c13aa9e788beb179ee575a3bff3
Agnogh <agnogh_orion@yahoo.com>
2025-08-06 00:52:56 +0200

    adding loop to repeat adding values until conditions are met

--- run.py
+++ run.py
@@ -49,24 +49,29 @@ def get_sales_data():
     """
     get sales figures input from user
     """
-    print("Please enter sales data from the last market")
-    print("Data should be 6 number seperated by commas")
-    print("Example: 10. 20, 30, 40, 50, 60\n")
+    while True:     # as long as it is "true" run
+        print("Please enter sales data from the last market")
+        print("Data should be 6 number seperated by commas")
+        print("Example: 10. 20, 30, 40, 50, 60\n")
 
-# data_str is assigned value to wahatever user adds after
-# the text "Enter your..."
-    data_str = input("Enter your Data here: ")
+    # data_str is assigned value to wahatever user adds after
+    # the text "Enter your..."
+        data_str = input("Enter your Data here: ")
 
-# sales_data is assigned value previously assigned "data_str" but it
-# is seperated by comma (,)
-    sales_data = data_str.split(",")
-# this is taking list of values user added in step before and send it to
-# 'validate_data' function
-    validate_data(sales_data)
-# prints value of "sales_data"
-    print(sales_data)
+    # sales_data is assigned value previously assigned "data_str" but it
+    # is seperated by comma (,)
+        sales_data = data_str.split(",")
+    # this is taking list of values user added in step before and send it to
+    # 'validate_data' function
+        validate_data(sales_data)
+    # prints value of "sales_data"
+        print(sales_data)
 
-    print(f'The Data provided in the {data_str}')
+        print(f'The Data provided in the {data_str}')
+
+        if validate_data(sales_data):       # run the "validate_data" func
+            print("Data is valid!")     # print statement
+            break       # break out of the "while" loop
 
 
 def validate_data(values):
@@ -75,13 +80,20 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values
     """
+# print(values) was jsut for checking the code
     try:
+        # this line of code is used to convert value to integers
+        # for each element in 'values'
+        [int(value) for value in values]
         if len(values) != 6:    # if the length/number of value is NOT 6
             raise ValueError(   # raise error,text will show up insted
                 f'Exactly 6 values requiered, you provided  {len(values)}'
             )
     except ValueError as e:     # except the value error as "e" and print out
-        print(f'Invalid data: {e}, please try again\n')
+        print(f'Invalid data: {e}, try again with actual numbers\n')
+        return False        # return false as validation failed
+    # return "true" which means data is valid
+    return True
 
     print(values)
 
