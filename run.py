42f41e3e025068f8e42421e0e5328eac0e04e3c2
Agnogh <agnogh_orion@yahoo.com>
2025-08-07 01:49:40 +0200

    Retrive last stock data from numbers spreadsheet

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+# from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -111,6 +112,33 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully. \n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+
+    The surplus is defined as the sales figure subtracted from the stock:
+    - positive surplus inidcates waste
+    - negative surplus indicate extra made when the stocks was sold out.
+    """
+    print("Calculating surplus data...\n")
+    # getting the data from Stock sheet and get all values
+    stock = SHEET.worksheet("stock").get_all_values()
+    # pprint(stock)
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    run all program function
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    # calling the function "calculate_surplus_data" with sales_data variable
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to Love sandiches data automation")
+# calling out my main function
+main()
